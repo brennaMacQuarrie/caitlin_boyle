121b50864ec6e8f56d8d65a15237589395593fe9
brennaMacQuarrie <brennamacquarrie@gmail.com>
2022-04-19 10:16:17 -0600

    move eye

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const modal = document.getElementById('modal')
+const menuCTA = document.getElementById('show-modal')
+const mainEye = document.getElementById('show-photos')
+const smallEye = document.getElementById('eye-sm')
+
 function changeColor() {
-    // document.querySelector('.coming-soon').classList.add('set')
-    document.querySelector('.eye').classList.add('set')
+    mainEye.classList.add('set')
 
     const lines = document.querySelectorAll('.line')
     for (const line of lines) {
@@ -12,15 +16,16 @@ document.onreadystatechange = function() {
     setTimeout(changeColor, 700)
 }
 
-document.getElementById('show-modal').onclick = function() {
-    document.getElementById('modal').classList.toggle('open')
+menuCTA.onclick = function() {
+    modal.classList.toggle('open')
     document.getElementById('hello').classList.toggle('open')
-    document.getElementById('show-modal').classList.toggle('open')
+    menuCTA.classList.toggle('open')
 }
 
-document.getElementById('show-photos').onclick = function() {
-    document.getElementById('show-photos').classList.toggle('corner')
-
+mainEye.onclick = function() {
+    mainEye.classList.add('hidden')
+    smallEye.classList.remove('invisible')
+    // show photos now!
 }
 
 
